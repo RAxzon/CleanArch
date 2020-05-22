bfd203ca3180c59e22aa70dbdf4319a3bdd96b3f
Rikard Axelsson <nousetocare@hotmail.com>
2020-05-22 15:30:43 +0200

    Implementation of Service and Repository

---
+++ CleanArch/CleanArch.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain1.Interfaces;
+using CleanArch.Domain1.Models;
+using CleanArch.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infrastructure.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+
+        private UniversityDBContext _context;
+
+        public CourseRepository(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
