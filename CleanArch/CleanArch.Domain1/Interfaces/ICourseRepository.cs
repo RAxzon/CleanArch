72506ab4006c498df1690f73905759cd2cb6ce46
Rikard Axelsson <nousetocare@hotmail.com>
2020-05-22 15:08:05 +0200

    Added Interfaces Service and Repository

---
+++ CleanArch/CleanArch.Domain1/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Domain1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain1.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
